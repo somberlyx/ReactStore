ae0e38e0d6fb82546e1697699d20a8db9d55a996
somberlyx <lybasiddiqui02@gmail.com>
2024-01-09 19:42:06 +0400

    Create Header component and insert it where necessary

--- myntra-react-clone/src/components/Bag.jsx
+++ myntra-react-clone/src/components/Bag.jsx
@@ -1,53 +1,9 @@
+import Header from "./Header";
+
 const Bag = () => {
   return (
     <>
-      <header>
-        <div className="logo_container">
-          <a href="#">
-            <img
-              className="myntra_home"
-              src="../images/myntra_logo.webp"
-              alt="Myntra Home"
-            />
-          </a>
-        </div>
-        <nav className="nav_bar">
-          <a href="#">Men</a>
-          <a href="#">Women</a>
-          <a href="#">Kids</a>
-          <a href="#">Home & Living</a>
-          <a href="#">Beauty</a>
-          <a href="#">
-            Studio <sup>New</sup>
-          </a>
-        </nav>
-        <div className="search_bar">
-          <span className="material-symbols-outlined search_icon">search</span>
-          <input
-            className="search_input"
-            placeholder="Search for products, brands and more"
-          />
-        </div>
-        <div className="action_bar">
-          <div className="action_container">
-            <span className="material-symbols-outlined action_icon">person</span>
-            <span className="action_name">Profile</span>
-          </div>
-
-          <div className="action_container">
-            <span className="material-symbols-outlined action_icon">favorite</span>
-            <span className="action_name">Wishlist</span>
-          </div>
-
-          <div className="action_container">
-            <span className="material-symbols-outlined action_icon">
-              shopping_bag
-            </span>
-            <span className="action_name">Bag</span>
-            <span className="bag-item-count">0</span>
-          </div>
-        </div>
-      </header>
+      <Header />
       <main>
         <div className="bag-page">
           <div className="bag-items-container"></div>
