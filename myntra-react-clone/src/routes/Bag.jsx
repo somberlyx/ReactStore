acb1ecc611ad585d989717489d8185f0c16fc6e7
somberlyx <lybasiddiqui02@gmail.com>
2024-01-13 20:12:45 +0400

    Create component to display price summary

--- myntra-react-clone/src/routes/Bag.jsx
+++ myntra-react-clone/src/routes/Bag.jsx
@@ -1,9 +1,11 @@
+import BagSummary from "../components/BagSummary";
+
 const Bag = () => {
   return (
     <main>
       <div className="bag-page">
         <div className="bag-items-container"></div>
-        <div className="bag-summary"></div>
+        <BagSummary />
       </div>
     </main>
   );
